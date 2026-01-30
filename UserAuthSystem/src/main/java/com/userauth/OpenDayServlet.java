62b8831b9be7973376f6bc93d58412b4413f1fc4
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-01-30 09:15:51 +0530

    Delete UserAuthSystem directory

--- UserAuthSystem/src/main/java/com/userauth/OpenDayServlet.java
+++
@@ -1,9 +0,0 @@
-@WebServlet("/openDay")
-public class OpenDayServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String id = req.getParameter("id");
-
-        // redirect to single JSP page
-        resp.sendRedirect("openDay.jsp?id=" + id);
-    }
-}
