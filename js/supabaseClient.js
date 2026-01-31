660499f6dfd6dcf4b47669819a10c153aa5f7fb3
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-01-31 18:19:40 +0530

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 
-const SUPABASE_URL = https://woysaoheokhipqpfphpr.supabase.co;
-const SUPABASE_KEY = sb_publishable_WbweOMELckXb1bfnEA-g9A_YF3NwmSR;
+const SUPABASE_URL = "https://woysaoheokhipqpfphpr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_WbweOMELckXb1bfnEA-g9A_YF3NwmSR";
 
 const { createClient } = supabase;
 window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
