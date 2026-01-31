44abebb5059f9cd1f0a6a8a8f591df9935cad1c3
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-01-31 22:18:11 +0530

    Update uiRenderer.js

--- js/uiRenderer.js
+++ js/uiRenderer.js
@@ -1,46 +1,14 @@
-// js/uiRenderer.js
-
-function renderBreadcrumb(path) {
-  const container = document.getElementById("breadcrumb");
-  const parts = path.split("/").filter(Boolean);
-
-  let html = `<span data-path="">My Drive</span>`;
-  let cumulative = "";
-
-  parts.forEach(part => {
-    cumulative += part + "/";
-    html += ` > <span data-path="${cumulative}">${part}</span>`;
-  });
-
-  container.innerHTML = html;
-}
-
-function renderFiles(files, currentPath, userId) {
-  const grid = document.getElementById("fileGrid");
-  grid.innerHTML = "";
+function renderFiles(files) {
+  const container = document.getElementById("fileContainer");
+  container.innerHTML = "";
 
   files.forEach(file => {
-    const fullPath = `${userId}/${currentPath}${file.name}`;
-    const ext = file.name.split(".").pop().toLowerCase();
-
-    let preview;
-
-    if (["png", "jpg", "jpeg", "webp"].includes(ext)) {
-      preview = `<img src="${getPublicUrl(fullPath)}" class="file-thumb">`;
-    } else if (ext === "pdf") {
-      preview = `<div class="file-icon material-icons">picture_as_pdf</div>`;
-    } else {
-      preview = `<div class="file-icon material-icons">insert_drive_file</div>`;
-    }
-
-    grid.innerHTML += `
-      <div class="file-card" data-name="${file.name}">
-        ${preview}
-        <div class="file-footer">
-          <span>${file.name}</span>
-          <span class="material-icons more-btn">more_vert</span>
-        </div>
-      </div>
+    const div = document.createElement("div");
+    div.className = "file-card";
+    div.innerHTML = `
+      <div>${file.name}</div>
+      <button onclick="showMenu('${file.id}','${file.path}')">⋮</button>
     `;
+    container.appendChild(div);
   });
 }
