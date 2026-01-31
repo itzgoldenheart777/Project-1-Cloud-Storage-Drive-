52bf32f9fca6d96a9462a2bd5249c689a6efc27f
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-01-31 20:53:49 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,16 +2,13 @@ async function forgotPassword() {
   const email = document.getElementById("email").value;
 
   if (!email) {
-    alert("Enter your email first.");
+    alert("Please enter your email first.");
     return;
   }
 
-  const { error } = await window.supabaseClient.auth.resetPasswordForEmail(
-    email,
-    {
-      redirectTo: window.location.origin + "/reset.html"
-    }
-  );
+  const { error } = await window.supabaseClient.auth.resetPasswordForEmail(email, {
+    redirectTo: "https://itzgoldenheart777.github.io/Project-1-Cloud-Storage-Drive/reset.html"
+  });
 
   if (error) {
     alert(error.message);
