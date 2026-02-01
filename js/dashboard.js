968f733e76557bd63cc7a16b438436e64fa62867
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-02-01 21:41:44 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,10 @@
 const supabaseClient = supabase.createClient(
-  "https://YOUR_PROJECT_ID.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
+  "https://woysaoheokhipqpfphpr.supabase.co",
+  "sb_publishable_WbweOMELckXb1bfnEA-g9A_YF3NwmSR"
 );
+
+//const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndveXNhb2hlb2toaXBxcGZwaHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDg0NDcsImV4cCI6MjA4NTMyNDQ0N30.IuWML5hwlXmo6yONo5JaYfzsypkajyxZ29sfyjaRqcA";
+
 
 let currentUser = null;
 
